515da54f6bb300402e41ad9c609579ed632da2ff
senkenn <senken32@gmail.com>
2024-11-11 22:16:57 +0900

    Add DuckDB and Apache Arrow dependencies; implement data querying in App component

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -9,6 +9,15 @@ import {
   YAxis,
 } from "recharts";
 import "./App.css";
+import { useEffect, useState } from "react";
+
+import * as duckdb from "@duckdb/duckdb-wasm";
+import eh_worker from "@duckdb/duckdb-wasm/dist/duckdb-browser-eh.worker.js?url";
+import mvp_worker from "@duckdb/duckdb-wasm/dist/duckdb-browser-mvp.worker.js?url";
+import duckdb_wasm_eh from "@duckdb/duckdb-wasm/dist/duckdb-eh.wasm?url";
+import duckdb_wasm from "@duckdb/duckdb-wasm/dist/duckdb-mvp.wasm?url";
+
+import type { Int } from "apache-arrow";
 
 const data = [
   {
@@ -55,10 +64,54 @@ const data = [
   },
 ];
 
+const MANUAL_BUNDLES: duckdb.DuckDBBundles = {
+  mvp: {
+    mainModule: duckdb_wasm,
+    mainWorker: mvp_worker,
+  },
+  eh: {
+    mainModule: duckdb_wasm_eh,
+    mainWorker: eh_worker,
+  },
+};
+
 function App() {
+  const [result, setResult] = useState<any>(null);
+
+  useEffect(() => {
+    (async () => {
+      // Select a bundle based on browser checks
+      const bundle = await duckdb.selectBundle(MANUAL_BUNDLES);
+      if (bundle.mainWorker === null) {
+        console.error("No bundle available for this browser");
+        return;
+      }
+      // Instantiate the asynchronous version of DuckDB-wasm
+      const worker = new Worker(bundle.mainWorker);
+      const logger = new duckdb.ConsoleLogger();
+      const db = new duckdb.AsyncDuckDB(logger, worker);
+      await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
+
+      const conn = await db.connect();
+      setResult(
+        await conn.query<{ v: Int }>(
+          "SELECT count(*)::INTEGER as v FROM generate_series(0, 100) t(v)",
+        ),
+      );
+
+      await conn.close();
+      await db.terminate();
+      await worker.terminate();
+    })();
+  }, []);
+
   return (
     <>
       <h2> Weather Forecast Comparison</h2>
+
+      <pre style={{ textAlign: "left" }}>
+        {JSON.stringify(result, undefined, 2)}
+      </pre>
 
       <ResponsiveContainer width={"101%"} height={300}>
         <LineChart data={data}>
