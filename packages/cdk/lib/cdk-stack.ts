a9ba3b3e89c8edf83a446a0a133919488205aace
senkenn <senken32@gmail.com>
2024-11-30 23:21:44 +0900

    Add CORS configuration to S3 bucket for weather data storage

--- packages/cdk/lib/cdk-stack.ts
+++ packages/cdk/lib/cdk-stack.ts
@@ -1,21 +1,20 @@
 import * as cdk from "aws-cdk-lib";
-import { Bucket } from "aws-cdk-lib/aws-s3";
+import { Bucket, HttpMethods } from "aws-cdk-lib/aws-s3";
 import type { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class S3Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-
     new Bucket(this, "MyBucket", {
       bucketName: "weather-forecast-comparison-data-store",
+      cors: [
+        {
+          allowedMethods: [HttpMethods.GET],
+          allowedOrigins: ["*"],
+          allowedHeaders: ["*"],
+        },
+      ],
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
   }
